a13f8672bb78c688b7c45269f6fa8230cf922e79
hyeongmin90 <nsby8882@naver.com>
2025-05-27 00:14:54 +0900

    [Gold V] Title: MooTube (Silver), Time: 184 ms, Memory: 2404 KB -BaekjoonHub

--- 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.cc
+++ 백준/Gold/15591. MooTube （Silver）/MooTube （Silver）.cc
@@ -2,28 +2,28 @@
 
 using namespace std;
 
-int usado[5001][5001];
-vector<int> adj[5001];
+vector<pair<int, int>> adj[5001];
 queue<pair<int, int>> q;
 int n;
 
-void func(){
-    for(int i = 1; i <= n; i++){
-        bool visit[5001] = {};
-        visit[i] = 1;
-        q.push({i, 1000000001});
-        while(!q.empty()){
-            auto [cur, u] = q.front();
-            q.pop();
-            for(auto nxt : adj[cur]){
-                if(visit[nxt]) continue;
-                int min_val = min(u, usado[cur][nxt]);
-                if(usado[i][nxt] == 0) usado[i][nxt] = min_val;
-                visit[nxt] = 1;
-                q.push({nxt, min_val});
-            }
+int func(int k, int i){
+    int result = 0;
+    bool visit[5001] = {};
+    visit[i] = 1;
+    q.push({i, k});
+    while(!q.empty()){
+        auto [cur, u] = q.front();
+        q.pop();
+        for(auto [nxt, usado] : adj[cur]){
+            if(visit[nxt]) continue;
+            int min_val = min(u, usado);
+            if(min_val < k) continue;
+            result++;
+            visit[nxt] = 1;
+            q.push({nxt, min_val});
         }
     }
+    return result;
 }
 
 int main(){
@@ -33,19 +33,13 @@ int main(){
     for(int i = 0; i < n - 1; i++){
         int u, v, r;
         cin >> u >> v >> r;
-        usado[u][v] = r;
-        usado[v][u] = r;
-        adj[u].push_back(v);
-        adj[v].push_back(u);
+        adj[u].push_back({v, r});
+        adj[v].push_back({u, r});
     }
-    func();
+
     for(int i = 0; i < q; i++){
         int k, v;
         cin >> k >> v;
-        int cnt = 0;
-        for(int i = 1; i <= n; i++){
-            if(usado[v][i] >= k) cnt++;
-        }
-        cout << cnt << '\n';
+        cout << func(k, v) << '\n';
     }
 }
