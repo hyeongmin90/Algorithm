5c5b2883ff1a300e6c93edf7c95314da1a6a4fb5
hyeongmin90 <nsby8882@naver.com>
2025-05-27 00:45:05 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int a[51];
+int b[51];
+
+int main(){
+    int n; cin >> n;
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    
+    for(int i = 0; i < n; i++)
+        cin >> b[i];
+
+    sort(a, a + n);
+    sort(b, b + n, greater<>());
+    int ans = 0;
+
+    for(int i = 0; i < n; i++)
+        ans += a[i] * b[i];
+
+    cout << ans << '\n';
+}
