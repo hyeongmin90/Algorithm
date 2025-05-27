36fb2a29a88851332ddfac85c7193acdfbfc3d67
hyeongmin90 <nsby8882@naver.com>
2025-05-27 10:45:44 +0900

    [Silver V] Title: 팩토리얼 0의 개수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1676. 팩토리얼 0의 개수/팩토리얼 0의 개수.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int N, cnt = 0;
+	cin >> N;
+	for (int i = 1; i <= N; i++) {
+		if (i % 5 == 0)cnt++;
+		if (i % 25 == 0)cnt++;
+		if (i % 125 == 0)cnt++;
+	}
+	cout << cnt;
+	return 0;
+}
