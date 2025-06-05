da3f8081169868c70f112cacac4731830359ebb3
PARK HYEONGMIN <nsby8882@naver.com>
2025-06-05 18:20:48 +0900

    [Silver I] Title: 부분수열의 합, Time: 4 ms, Memory: 3972 KB -BaekjoonHub

--- 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
+++ 백준/Silver/14225. 부분수열의 합/부분수열의 합.cc
@@ -3,38 +3,27 @@
 using namespace std;
 
 int arr[21];
-bool vis[21];
+bool check[2000001];
 int n;
 unordered_set<int> s;
 
-void dfs(int depth, int j, int sum, int k){
-    if(depth == k){
-        s.insert(sum);
-        return;
-    }
+void dfs(int depth, int j, int sum){
+    check[sum] = true;
+    if(depth == n) return;
     for(int i = j; i < n; i++){
-        if(vis[i]) continue;
-        vis[i] = 1;
-        dfs(depth + 1, i + 1, sum + arr[i], k);
-        vis[i] = 0;
+        dfs(depth + 1, i + 1, sum + arr[i]);
     }
 }
 
 int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> n;
     for(int i = 0; i < n; i++){
         cin >> arr[i];
     }
-    for(int i = 1; i <= n; i++){
-        dfs(0, 0, 0, i);
-    }
+    dfs(0, 0, 0);
 
     int a = 1;
-    while(1){
-        if(s.find(a) == s.end()){
-            cout << a << '\n';
-            break;
-        }
-        a++;
-    }
+    while(check[a]) a++;
+    cout << a << '\n';
 }
