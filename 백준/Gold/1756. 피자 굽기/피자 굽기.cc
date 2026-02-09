ac101f34b8816e9ceada1a196e46ec7d8ea1794d
PARK HYEONGMIN <nsby8882@naver.com>
2026-02-09 11:38:43 +0900

    [Gold V] Title: 피자 굽기, Time: 60 ms, Memory: 5220 KB -BaekjoonHub

--- 백준/Gold/1756. 피자 굽기/피자 굽기.cc
+++ 백준/Gold/1756. 피자 굽기/피자 굽기.cc
@@ -6,6 +6,7 @@ int d, n;
 vector<int> oven;
 
 int main(){
+    ios::sync_with_stdio(0), cin.tie(0), cout.tie(0);
     cin >> d >> n;
     int min_wide = INT_MAX;
     for(int i = 0; i < d; i++){
@@ -15,15 +16,20 @@ int main(){
         oven.push_back(min_wide);
     }
     auto e = oven.end();
+    int ans = 0;
     for(int i = 0; i < n; i++){
         int a;
         cin >> a;
-        auto it = upper_bound(oven.begin(), e, a, greater<int>()) - 1;
-        int idx = it - oven.begin();
-        
-        if(i == n - 1){
-            cout << idx + 1<< '\n';
+        auto it = upper_bound(oven.begin(), e, a, greater<int>());
+
+        if (it == oven.begin()) {
+            cout << 0 << '\n';
+            return 0;
         }
+        it--;
+        int idx = it - oven.begin();
+        ans = idx + 1;
         e = it;
     }
+    cout << ans << '\n';
 }
